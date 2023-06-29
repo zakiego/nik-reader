dde5ecbb2c6e27f05b7afcec8e09f1f6ecd766bb
zakiego <belummikir@gmail.com>
2023-06-29 15:57:01 +0800

    feat: add title

--- pages/index.tsx
+++ pages/index.tsx
@@ -9,8 +9,14 @@ import { Feature } from "~/lib/const";
 export default function Home() {
   const [selectedTab, setSelectedTab] = useState<Feature>("read");
 
+  const title = match(selectedTab)
+    .with("read", () => "Baca NIK")
+    .with("predict", () => "Prediksi NIK")
+    .otherwise(() => "NIK");
+
   return (
     <div className="px-4 py-9 max-w-3xl mx-auto">
+      <title>{title}</title>
       <Navbar />
       <Tab selectedTab={selectedTab} setSelectedTab={setSelectedTab} />
       {match(selectedTab)
