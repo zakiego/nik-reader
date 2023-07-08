8cb699475d18812818c0739b3d7a4667cb62da3c
zakiego <belummikir@gmail.com>
2023-07-08 16:36:48 +0800

    feat: remove image on auth page

--- pages/login.tsx
+++ pages/login.tsx
@@ -49,7 +49,7 @@ const Login = () => {
   return (
     <>
       <title>Login</title>
-      <div className="flex min-h-screen flex-1 bg-gray-950">
+      <div className="flex min-h-screen flex-1 bg-gray-950 justify-center">
         <div className="flex flex-1 flex-col justify-center px-4 py-12 sm:px-6 lg:flex-none lg:px-20 xl:px-24">
           <div className="mx-auto w-full max-w-sm lg:w-96">
             <div className="text-white space-y-3">
@@ -75,7 +75,7 @@ const Login = () => {
                 <div className="mb-4">
                   {errorMessages && <Alert message={errorMessages} />}
                 </div>
-                <form className="space-y-6">
+                <form className="space-y-6" autoComplete="off">
                   <div>
                     <label
                       htmlFor="secretCode"
@@ -88,7 +88,6 @@ const Login = () => {
                         id="secretCode"
                         name="secretCode"
                         type="password"
-                        autoComplete="off"
                         placeholder="Enter your secret code"
                         className="block w-full rounded-md border-0 py-1.5 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-gray-600 sm:text-sm sm:leading-6"
                         onChange={(e) => setSecretCode(e.target.value)}
@@ -109,13 +108,6 @@ const Login = () => {
               </div>
             </div>
           </div>
-        </div>
-        <div className="relative hidden w-0 flex-1 lg:block">
-          <img
-            className="absolute inset-0 h-full w-full object-cover"
-            src="https://images.unsplash.com/photo-1496917756835-20cb06e75b4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1908&q=80"
-            alt=""
-          />
         </div>
       </div>
     </>
