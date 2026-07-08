99e9ac2a889ba2de76fa62c973f961e252498a35
M. Zakiyuddin Munziri <zakiyuddinmunziri@gmail.com>
2026-07-08 11:17:08 +0800

    feat(nik): centralize NIK validation in shared schema module

--- server/routers/nik.ts
+++ server/routers/nik.ts
@@ -1,22 +1,9 @@
-import { z } from "zod";
-
-import {
-  createTRPCRouter,
-  privateProcedure,
-  publicProcedure,
-} from "~/server/trpc";
+import { nikSchema } from "~/lib/nik-schema";
+import { createTRPCRouter, publicProcedure } from "~/server/trpc";
 import { extractDataFromNIK } from "~/utils/read";
 
 export const nikRouter = createTRPCRouter({
-  read: publicProcedure
-    .input(
-      z.object({
-        nik: z.coerce.string().length(16),
-      }),
-    )
-    .mutation(async ({ input }) => {
-      const { nik } = input;
-
-      return extractDataFromNIK(nik);
-    }),
+  read: publicProcedure.input(nikSchema).mutation(async ({ input }) => {
+    return extractDataFromNIK(input.nik);
+  }),
 });
